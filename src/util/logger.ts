a177e851fb73ae0e5d45ace5f0c89cf1e01bab0d
Christian Nuss <christian.nuss@gmail.com>
2025-06-28 14:12:36 -0400

    cleanup

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export interface Logger {
-  trace?: (...content: unknown[]) => void;
-  debug?: (...content: unknown[]) => void;
-  info?: (...content: unknown[]) => void;
-  warn?: (...content: unknown[]) => void;
-  error?: (...content: unknown[]) => void;
+  log?(message?: any, ...optionalParams: any[]): void;
+  debug?(message?: any, ...optionalParams: any[]): void;
+  info?(message?: any, ...optionalParams: any[]): void;
+  warn?(message?: any, ...optionalParams: any[]): void;
+  error?(message?: any, ...optionalParams: any[]): void;
+  trace?(message?: any, ...optionalParams: any[]): void;
 }
